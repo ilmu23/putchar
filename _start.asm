c010b9114eb418d8b60fb8bf81b3d52f03619a5c
ivalimak <ivalimak@student.hive.fi>
2025-01-29 17:22:16 +0200

    Add support for printing multiple args

--- _start.asm
+++ _start.asm
@@ -6,17 +6,29 @@ _start:
 	pop %rdi
 	mov %rsp,%rsi
 	and $-0xF,%rsp
-	add $8,%rsi
-	mov (%rsi),%r8
+	mov	$0,%r10
+	push %rsi
+
+_next_arg:
+	pop %r11
+	add $8,%r11
+	mov (%r11),%r8
 	mov $0,%r9
 	cmp %r8,%r9
-	je _exit
+	je _nl
+	add $1,%r10
+	push %r11
+	mov $1,%r9
+	cmp %r10,%r9
+	je _putstr
+	movb $32,%dil
+	call ft_putchar
 
 _putstr:
 	mov $0,%r9b
 	movb (%r8),%dil
 	cmp %r9b,%dil
-	je _nl
+	je _next_arg
 	call ft_putchar
 	mov $-1,%r9
 	cmp %r9,%rax
@@ -25,6 +37,9 @@ _putstr:
 	jmp _putstr
 
 _nl:
+	mov $0,%r9
+	cmp %r9,%r10
+	je _exit
 	movb $10,%dil
 	call ft_putchar
 
